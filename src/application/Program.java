fd526f61e2d0c5c4ecba4006fffed509e50361c3
unknown <jrenatomt@gmail.com>
2019-10-19 02:01:27 -0300

    criada classe vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 
      Departamento obj = new Departamento(2, "Renato");
-     System.out.println(obj);
+     Vendedor vendedor = new Vendedor(21, "renato", "jrenatomt@gmail.com", new Date(), 3000.00, obj);
+     System.out.println(vendedor);
 
 	}
 
