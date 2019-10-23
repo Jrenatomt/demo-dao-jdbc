064107f1274f4852f2333378d5a81fd342921858
unknown <jrenatomt@gmail.com>
2019-10-23 01:20:51 -0300

    implemetação FindByDepartamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
@@ -10,9 +13,16 @@ public class Program {
 
 		VendedorDao vd = DaoFactory.createVendedorDao();
 
+		System.out.println("========= teste 1 Vendedor FindById ==========");
 		Vendedor vendedor = vd.findById(3);
-
 		System.out.println(vendedor);
+		
+		System.out.println("/n========= Teste 2 Vendedor FindByDepartamento ==========");
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> list = vd.findByDepartamento(departamento);
+		for (Vendedor obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
